513634e5781985a2948384d97fcc59af07c2a799
Sasha <Sasha@DESKTOP-3MO0VVJ>
2023-09-05 00:05:25 +0300

    Добавьте файлы проекта.

---
+++ Yggdrasil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yggdrasil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
